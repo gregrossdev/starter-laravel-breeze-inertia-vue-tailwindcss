82b5d74767004de26d9f6430f4015083e324307e
Gregory Ross <gregross.dev@gmail.com>
2023-06-28 11:12:36 -0400

    make Head and Link components from inertia globally available

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
-
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
@@ -13,6 +12,8 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .component("Head", Head)
+            .component("Link", Link)
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
